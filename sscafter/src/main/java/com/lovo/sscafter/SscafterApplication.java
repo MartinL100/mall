2ab344bc9f9b6cbaf8d30ce6f1f891574f1b0c68
MartinCong <1246456207@qq.com>
2019-07-31 16:41:00 +0800

    Initial commit

---
+++ sscafter/src/main/java/com/lovo/sscafter/SscafterApplication.java
@@ -0,0 +1,13 @@
+package com.lovo.sscafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscafterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SscafterApplication.class, args);
+    }
+
+}
---
+++ sscbfore/src/main/java/com/lovo/sscbfore/SscbforeApplication.java
@@ -0,0 +1,13 @@
+package com.lovo.sscbfore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscbforeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SscbforeApplication.class, args);
+    }
+
+}
