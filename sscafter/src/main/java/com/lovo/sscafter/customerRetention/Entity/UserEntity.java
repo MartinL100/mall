faafe4828f7a6122405e67d066a58804fb5da458
19970703123 <807598097@qq.com>
2019-08-02 15:39:54 +0800

    fmx 2019/8/2 在sscafter中增加一个customerRetention

---
+++ sscafter/src/main/java/com/lovo/sscafter/customerRetention/Entity/UserEntity.java
@@ -0,0 +1,46 @@
+package com.lovo.sscafter.customerRetention.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String trueName1;
+    private  String userName1;
+    private String password1;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTrueName1() {
+        return trueName1;
+    }
+
+    public void setTrueName1(String trueName1) {
+        this.trueName1 = trueName1;
+    }
+
+    public String getUserName1() {
+        return userName1;
+    }
+
+    public void setUserName1(String userName1) {
+        this.userName1 = userName1;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+}
---
+++ sscafter/src/main/java/com/lovo/sscafter/customerRetention/dao/IUserDao.java
@@ -0,0 +1,12 @@
+package com.lovo.sscafter.customerRetention.dao;
+
+
+import com.lovo.sscafter.customerRetention.Entity.UserEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IUserDao extends CrudRepository<UserEntity, Integer> {
+    @Query(" from UserEntity where userName1=?1 and password1=?2")
+    public UserEntity findByUserName1AAndPassword1(String userName1,String password1);
+
+}
