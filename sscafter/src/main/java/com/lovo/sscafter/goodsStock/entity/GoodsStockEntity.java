f4816b0bd64e77b813529b5b25dfffcab8bbe07b
dinosaurTHJ <707552155@qq.com>
2019-08-04 15:50:40 +0800

    easyUI2

--- sscafter/src/main/java/com/lovo/sscafter/goodsStock/entity/GoodsStockEntity.java
+++ sscafter/src/main/java/com/lovo/sscafter/goodsStock/entity/GoodsStockEntity.java
@@ -27,4 +27,68 @@ private Long goodsNum;//商品库存
     @Column(name = "tag")
 private  String tag;//是否正在采购
 
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getGoodsType() {
+        return goodsType;
+    }
+
+    public void setGoodsType(String goodsType) {
+        this.goodsType = goodsType;
+    }
+
+    public String getGoodsNorms() {
+        return goodsNorms;
+    }
+
+    public void setGoodsNorms(String goodsNorms) {
+        this.goodsNorms = goodsNorms;
+    }
+
+    public String getGoodsUnit() {
+        return goodsUnit;
+    }
+
+    public void setGoodsUnit(String goodsUnit) {
+        this.goodsUnit = goodsUnit;
+    }
+
+    public Long getGoodsNum() {
+        return goodsNum;
+    }
+
+    public void setGoodsNum(Long goodsNum) {
+        this.goodsNum = goodsNum;
+    }
+
+    public long getGoodsMinNum() {
+        return goodsMinNum;
+    }
+
+    public void setGoodsMinNum(long goodsMinNum) {
+        this.goodsMinNum = goodsMinNum;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
