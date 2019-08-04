df2b7ccf2ef649d012c689e219f5f6ac702f1697
19960906 <248492911@qq.com>
2019-08-04 12:38:00 +0800

    czr 8-04

---
+++ sscafter/src/main/java/com/lovo/sscafter/promotionAndSalesReturn/salesReturn/service/imp/ReturnOrderServiceImp.java
@@ -0,0 +1,22 @@
+package com.lovo.sscafter.promotionAndSalesReturn.salesReturn.service.imp;
+
+import com.lovo.sscafter.promotionAndSalesReturn.salesReturn.dao.IReturnOrderDao;
+import com.lovo.sscafter.promotionAndSalesReturn.salesReturn.entity.ReturnOrderEntity;
+import com.lovo.sscafter.promotionAndSalesReturn.salesReturn.service.IReturnOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 退货订单service实现类
+ */
+@Service
+public class ReturnOrderServiceImp implements IReturnOrderService {
+
+    @Autowired
+    private IReturnOrderDao dao;
+
+    @Override
+    public void savaReturnOrder(ReturnOrderEntity orderEntity) {
+        dao.save(orderEntity);
+    }
+}
