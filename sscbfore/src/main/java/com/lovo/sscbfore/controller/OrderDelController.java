5c2cfc18e41a6111e491e64e85c2abf2f7a62b37
Ttxxll1996 <2573121139@qq.com>
2019-08-05 16:45:00 +0800

    唐祥龙

--- sscbfore/src/main/java/com/lovo/sscbfore/controller/OrderDelController.java
+++ sscbfore/src/main/java/com/lovo/sscbfore/controller/OrderDelController.java
@@ -16,10 +16,27 @@ import java.util.List;
 public class OrderDelController {
 
 
-    @RequestMapping("pageDel")
-    public String pageDel(){
-                String info="{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":" +
-                "[{\"ordernum\":J16820180805,\"ordertype\":\"退货中\",\"goodssize\":\"10\",\"orderdate\":\"2018-08-05\",\"paymethod\":\"支付宝\"}]}";
+    @RequestMapping("orderDel")
+    public String orderDel(){
+    String info="{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":" +
+                "[{\"ordernum\":\"J16820190801\",\"ordertype\":\"已完结\",\"goodssize\":\"10\",\"orderdate\":" +
+                "\"2018-08-01\",\"paymethod\":\"支付宝\",\"orderMoney\":\"21000元\"},{\"ordernum\":\"J16820190701\",\"ordertype\":" +
+                "\"待付款\",\"goodssize\":\"12\",\"orderdate\":\"2018-07-01\",\"paymethod\":" +
+                "\"预存款\",\"orderMoney\":\"10000元\"},{\"ordernum\":\"J16820190801\",\"ordertype\":\"已完结\",\"goodssize\":" +
+                "\"10\",\"orderdate\":\"2018-08-01\",\"paymethod\":\"银联支付\",\"orderMoney\":\"11000元\"},{\"ordernum\":\"J16820190808\",\"ordertype\":" +
+                "\"退货\",\"goodssize\":\"100\",\"orderdate\":\"2018-08-08\",\"paymethod\":\"支付宝\",\"orderMoney\":\"18000元\"}," +
+            "{\"ordernum\":\"J16820190601\",\"ordertype\":\"已完结\",\"goodssize\":\"50\",\"orderdate\":\"2018-06-01\",\"paymethod\":\"预存款\",\"orderMoney\":\"2000元\"}]}";
+        return info;
+    }
+    @RequestMapping("goodsDel")
+    public String goodsDel(){
+        String info="{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":" +
+                "[{\"goodsName\":\"Ak47-A\",\"goodsNum\":\"100支\",\"goodsPrice\":\"1000元/支\",\"goodsStatus\":" +
+                "\"已退货\"},{\"goodsName\":\"Ak47\",\"goodsNum\":" +
+                "\"100支\",\"goodsPrice\":\"1000元/支\",\"goodsStatus\":\"正常\"},{\"goodsName\":\"火麒麟\",\"goodsNum\":\"100支\",\"goodsPrice\":" +
+                "\"1000元/支\",\"goodsStatus\":\"正常\"},{\"goodsName\":\"激光\",\"goodsNum\":" +
+                "\"100支\",\"goodsPrice\":\"8888元/支\",\"goodsStatus\":\"退货中\"}," +
+                "{\"goodsName\":\"毁灭\",\"goodsNum\":\"100支\",\"goodsPrice\":\"8880元/支\",\"goodsStatus\":\"正常\"}]}";
         return info;
     }
 }
