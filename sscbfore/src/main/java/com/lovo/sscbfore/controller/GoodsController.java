4b72b99dac1bdf9eb40072c7258e2a23c461dbc2
MartinCong <1246456207@qq.com>
2019-08-10 15:33:26 +0800

    购物车

--- sscbfore/src/main/java/com/lovo/sscbfore/controller/GoodsController.java
+++ sscbfore/src/main/java/com/lovo/sscbfore/controller/GoodsController.java
@@ -17,18 +17,20 @@ public class GoodsController {
     //远程调用的模板
     @Autowired
     private RestTemplate restTemplate;
-    @RequestMapping("initGoods")
-    public String initGoods(){
-        //模拟预售
-        String info = "{\"total\":2,\"page\":1,\"rows\":[{\"goodsId\":\"ssss\",\"goodsName\":\"苹果\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"1.jpg\",\"goodsPrice\":\"100\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null},{\"goodsId\":\"1116\",\"goodsName\":\"苹果6\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"2.jpg\",\"goodsPrice\":\"130\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null}]}";
-        return  info;
-    }
+//    @RequestMapping("initGoods")
+//    public String initGoods(){
+//        //模拟预售
+//        String info = "{\"total\":2,\"page\":1,\"rows\":[{\"goodsId\":\"ssss\",\"goodsName\":\"苹果\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"1.jpg\",\"goodsPrice\":\"100\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null},{\"goodsId\":\"1116\",\"goodsName\":\"苹果6\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"2.jpg\",\"goodsPrice\":\"130\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null}]}";
+//        return  info;
+//    }
 
 @RequestMapping("initSelect")
     public String initSelect(){
 //        模拟获取远程下拉数据
-//       String info = restTemplate.getForEntity(UrlUtil.INIT_SELECT,String.class).getBody();
-        String info ="{\"SG\":\"水果\",\"YF\":\"衣服\",\"KZ\":\"裤子\"}";
+    String info = "";
+        info = restTemplate.getForEntity(UrlUtil.INIT_SELECT,String.class).getBody();
+    System.out.println(info);
+//        String info ="{\"SG\":\"水果\",\"YF\":\"衣服\",\"KZ\":\"裤子\"}";
         return info;
     }
     //查找预售商品
@@ -39,14 +41,14 @@ public class GoodsController {
         }if(StringUtils.isEmpty(goodsName)){
             goodsName="null";
         }if(StringUtils.isEmpty(page)){
-            page="null";
+            page="1";
         }if(StringUtils.isEmpty(limit)){
-            limit="null";
+            limit="10";
         }
-        String info = "{\"total\":2,\"page\":1,\"rows\":[{\"goodsId\":\"ssss\",\"goodsName\":\"预售香蕉\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"1.jpg\",\"goodsPrice\":\"100\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null},{\"goodsId\":\"1116\",\"goodsName\":\"库尔勒香梨\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"2.jpg\",\"goodsPrice\":\"130\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null}]}";
+
+        String info = "";
             String url = UrlUtil.FIND_GOODS_PRESELL+goodsType+"/"+goodsName+"/"+page+"/"+limit;
-                System.out.println(url);
-//        info = restTemplate.getForEntity(UrlUtil.FIND_GOODS_PRESELL+goodsType+"/"+goodsName+"/"+page+"/"+limit,String.class).getBody();
+        info = restTemplate.getForEntity(url,String.class).getBody();
         return info ;
     }
 
@@ -63,9 +65,9 @@ public class GoodsController {
             limit="null";
         }
         String info = "{\"total\":2,\"page\":1,\"rows\":[{\"goodsId\":\"ssss\",\"goodsName\":\"促销香蕉\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"1.jpg\",\"goodsPrice\":\"100\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null},{\"goodsId\":\"1116\",\"goodsName\":\"库尔勒香梨\",\"goodsType\":\"水果\",\"goodsNorms\":\"qqwee\",\"goodsUnit\":null,\"goodsBid\":12.12,\"goodsPath\":\"2.jpg\",\"goodsPrice\":\"130\",\"goodsDiscount\":12,\"goodsNum\":343,\"goodsState\":\"上架\",\"goodsBooking\":\"未预售\",\"promotionState\":null,\"shelfTime\":null,\"lowerTime\":null}]}";
-        String url = UrlUtil.FIND_GOODS_PRESELL+goodsType+"/"+goodsName+"/"+page+"/"+limit;
-        System.out.println(url);
-//        info = restTemplate.getForEntity(UrlUtil.FIND_GOODS_PRESELL+goodsType+"/"+goodsName+"/"+page+"/"+limit,String.class).getBody();
+
+
+        info = restTemplate.getForEntity(UrlUtil.FIND_GOODS_CUXIAO+goodsType+"/"+goodsName+"/"+page+"/"+limit,String.class).getBody();
         return info ;
     }
     //查找所有商品
