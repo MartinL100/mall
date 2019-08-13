1d393399fe76cb22f84b49c9824d7518a2a82a42
tanliang199610 <1064268863@qq.com>
2019-08-13 13:54:04 +0800

    谭亮提交

--- csc/src/main/java/com/lovo/csc/dao/salesReturnDao/IScopeOrderWSDao.java
+++ csc/src/main/java/com/lovo/csc/dao/salesReturnDao/IScopeOrderWSDao.java
@@ -3,6 +3,8 @@ package com.lovo.csc.dao.salesReturnDao;
 
 import com.lovo.csc.entity.salesReturnEntity.OrderEntity;
 import com.lovo.csc.entity.salesReturnEntity.ScopeOrderEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface IScopeOrderWSDao extends CrudRepository<ScopeOrderEntity,String
 ////    @Modifying
 ////    @Query("from OrderEntity o where o.scopeOrder.orderId=?1")
 //    public List<OrderEntity> findByScopeOrder(String orderId);
+//    @Modifying
+//  @Query("update ScopeOrderEntity set orderState=?1 where orderId=?2")
+//  public int updateScopeOrder(String orderState,String orderId);
 }
