37f450163c8474557d954954d34c39bc08247b52
Ttxxll1996 <2573121139@qq.com>
2019-08-13 14:43:08 +0800

    唐祥龙

--- sscbfore/src/main/java/com/lovo/sscbfore/controller/OrderController.java
+++ sscbfore/src/main/java/com/lovo/sscbfore/controller/OrderController.java
@@ -11,6 +11,7 @@ package com.lovo.sscbfore.controller;
         import org.springframework.web.client.RestTemplate;
 
         import javax.servlet.http.HttpServletRequest;
+        import java.io.IOException;
         import java.util.ArrayList;
         import java.util.List;
 
@@ -23,34 +24,27 @@ public class OrderController {
     //远程调用的模板
     @Autowired
     private RestTemplate restTemplate;
-    //根据订单号删除
-    @RequestMapping("orderNumDel")
-    public  String  orderDel(String orderNum){
 
-        String orders= restTemplate.getForEntity("http://sscafter/delOrderInfo/"+orderNum,String.class).getBody();
-        return orders;
-   }
 //根据下单日期,订单类型,用户名模糊查询
     @RequestMapping("orderDellist")
-    public String orderFindByDateAndTypeAndName(HttpServletRequest request, String orderDate, Integer orderType, int page , int limit)throws JsonProcessingException {
+    public String orderFindByDateAndTypeAndName(HttpServletRequest request, String orderDateList, Integer orderTypeList, String page , String limit)throws JsonProcessingException {
        //从session中获取登录的用户名
-//        String userName =((UserEntity)request.getSession().getAttribute("userEntity")).getUserName() ;
-        String userName="zy";
-        if("".equals(orderDate)||null==orderDate){
-            orderDate="no";
-        }
-        if("".equals(orderType)||null==orderType ){
-            orderDate="0";
-        }
-        //远程调用接口
-        String orderInfo= restTemplate.getForEntity("http://sscafter/findOrderInfo/"+orderDate+"/"+orderType+"/"+page+"/"+limit+"/"+userName,String.class).getBody();
+     String userName =((UserEntity)request.getSession().getAttribute("userEntity")).getUserName() ;
+//        System.out.printf(""+orderDateList+orderTypeList);
+        int page1 =Integer.parseInt(page);
+        int limit1 = Integer.parseInt(limit);
 
-//        List<> orderInfo="";
-//        try {
-//            orderInfo = objectMapper.readValues(order,List.class);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+
+        if("".equals(orderDateList)||null==orderDateList){
+            orderDateList="no";
+        }if("".equals(orderTypeList)||null==orderTypeList ){
+            orderTypeList=3;
+        }
+
+        String url = "http://sscafter/findOrderInfo/"+orderDateList+"/"+orderTypeList+"/"+page+"/"+limit+"/"+userName;
+
+        //远程调用接口
+        String orderInfo= restTemplate.getForEntity(url,String.class).getBody();
         orderInfo= "{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":"+orderInfo+"}";
 
         return orderInfo;
@@ -59,30 +53,29 @@ public class OrderController {
     //根据下单日期,订单类型,用户名 查询总行数
     @RequestMapping("orderDelNum")
     public int orderDateAndTypeAndNameNum(HttpServletRequest request,String orderDate, Integer orderType){
-//        String userName =((UserEntity)request.getSession().getAttribute("userEntity")).getUserName() ;        //远程调用接口
-        String userName="zy";
+     String userName =((UserEntity)request.getSession().getAttribute("userEntity")).getUserName() ;        //远程调用接口
         if("".equals(orderDate)||null==orderDate ){
             orderDate="no";
         }
         if("".equals(orderType)||null==orderType ){
-            orderDate="0";
+            orderType=3;
         }
         return Integer.parseInt(restTemplate.getForEntity("http://sscafter/findOrderRows/"+orderDate+"/"+orderType+"/"+userName,String.class).getBody());
     }
 //根据订单号查询商品的集合 进行分页
     @RequestMapping("orderNumlist")
-    public String orderFindByOrderNum(String orderNum,int pageTwo ,int limit){
+    public String orderFindByOrderNum(String orderNum,int page ,int limit){
 
-      String goodsDto=  restTemplate.getForEntity("http://sscafter/findGoodsRows/"+orderNum+"/"+pageTwo+"/"+limit,String.class).getBody();
-        String goodsInfo="";
+      String goodsDto=  restTemplate.getForEntity("http://sscafter/findGoodsRows/"+orderNum+"/"+page+"/"+limit,String.class).getBody();
+
 //        try {
 //            goodsInfo = objectMapper.writeValueAsString(goodsDto);
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-        goodsInfo= "{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":"+goodsInfo+"}";
+        goodsDto= "{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":"+goodsDto+"}";
 
-        return goodsInfo;
+        return goodsDto;
 
     }
 //根据订单号 查询商品的总行数
@@ -90,5 +83,13 @@ public class OrderController {
     public int goodsNum(String orderNum){
 
         return Integer.parseInt(restTemplate.getForEntity("http://sscafter/findGoodsRows/"+orderNum,String.class).getBody());
+    }
+
+    //根据订单号删除
+    @RequestMapping("orderNumDel")
+    public  String  orderDel(String orderNum){
+
+        String orders= restTemplate.getForEntity("http://sscafter/delOrderInfo/"+orderNum,String.class).getBody();
+        return orders;
     }
 }
